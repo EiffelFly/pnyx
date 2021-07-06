81f8c4ccb965ebbeb2625190b13e3596abeadb40
totuslink <summerbud@totuslink.com>
2021-07-06 22:41:39 +0800

    add FeaturesButton

---
+++ src/components/FeaturesButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const FeaturesButton: React.FC = () => {
+  return (
+    <button
+      className="font-sans font-light text-sd-black text-lg my-auto hover:bg-sd-brcyan hover:bg-opacity-20 px-2 py-0.5 rounded-md"
+    >
+      Features
+    </button>
+  )
+}
