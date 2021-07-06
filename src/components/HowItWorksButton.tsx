b7bad0e37510b895261b26d42dec0e4298aab87c
totuslink <summerbud@totuslink.com>
2021-07-06 22:41:26 +0800

    add HowItWorksButton

---
+++ src/components/HowItWorksButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const HowItWorksButton: React.FC = () => {
+  return (
+    <button
+      className="font-sans font-light text-sd-black text-lg my-auto hover:bg-sd-brcyan hover:bg-opacity-20 px-2 py-0.5 rounded-md"
+    >
+      How it works
+    </button>
+  )
+}
