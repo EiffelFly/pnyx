ae8de4fc05ae985bb9cf5502b11f96d43b5f4b09
totuslink <summerbud@totuslink.com>
2021-07-07 10:07:19 +0800

    add HowItWorksHeadline

---
+++ src/components/HowItWorksHeadline.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Header } from "./Header";
+import { SubHeader } from "./SubHeader";
+
+export const HowItWorksHeadline = () => {
+  return (
+    <div
+      className="flex flex-col"
+    >
+      <Header >
+        Take every conversation as whole picture
+      </Header>
+      <SubHeader>
+        Graph is powerful tool for discussion, it leverages everyone to involve into the dicussion deeper
+      </SubHeader>
+    </div>
+  )
+}
