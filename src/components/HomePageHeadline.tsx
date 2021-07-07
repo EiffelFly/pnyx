bae46bc15be88de1ccceb0e083acfad8f13b4d5f
totuslink <summerbud@totuslink.com>
2021-07-07 10:52:36 +0800

    add HomePageHeadline component

---
+++ src/components/HomePageHeadline.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Header } from "./Header";
+import { SubHeader } from "./SubHeader";
+
+interface Props {
+  className: string
+}
+
+export const HomePageHeadline: React.FC<Props> = ({ className }) => {
+  return (
+    <div
+      className={"flex flex-col gap-y-4 " +  className }
+    >
+      <Header >
+        Make great discussion occur,<br /> in graph
+      </Header>
+      <SubHeader>
+        Pnyx is a text discussion tool built for people who is seeking brilliant ideas and insights
+      </SubHeader>
+    </div>
+  )
+}
