0c1ba5ff9e6823b40bbd3deb5e33429e2e14c86f
totuslink <summerbud@totuslink.com>
2021-07-07 15:12:39 +0800

    add FeaturesHeadline component

---
+++ src/components/FeaturesHeadline.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { Header } from "./Header";
+import { SubHeader } from "./SubHeader";
+
+interface Props {
+  className: string;
+}
+
+export const FeaturesHeadline: React.FC<Props> = ({ className }) => {
+  return (
+    <div
+      className={"flex flex-col gap-y-4" + " " +  className }
+    >
+      <Header>
+        Built for cultivating briliant ideas
+      </Header>
+      <SubHeader>
+        Discuss in text is a powerful way to gain insight, it non-realtime feature 
+        provide us space for great ideas.
+      </SubHeader>
+    </div>
+  )
+}
