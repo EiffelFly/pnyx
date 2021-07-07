7b45c8b2de52f9740af3f19963e74a499d5458bb
totuslink <summerbud@totuslink.com>
2021-07-07 21:37:30 +0800

    fix pnyxlogo hover issue

--- src/components/buttons/PynxLogoButton.tsx
+++ src/components/buttons/PynxLogoButton.tsx
@@ -1,21 +1,37 @@
 import * as React from "react";
+import { withRouter } from "react-router-dom";
 
-interface Props {}
+interface Props { }
 
-export const PynxLogoButton: React.FC<Props> = () => {
+export const PynxLogoButton = withRouter(({ history }) => {
   const [hovered, setHovered] = React.useState(false);
-  const toggleHover = () => setHovered(!hovered);
+  const [refreshed, setRefreshed] = React.useState(false);
+  const toggleHover = () => {
+    if (refreshed) {
+      setRefreshed(false);
+      return
+    }
+    setHovered(!hovered)
+  };
   return (
     <button
-      className={hovered ? 
-      "mr-auto my-auto text-3xl font-sans font-extrabold transition-colors duration-500 ease-linear text-transparent bg-clip-text bg-gradient-to-br from-sd-blue to-sd-cyan"
-      :
-      "mr-auto my-auto text-3xl font-sans font-extrabold text-sd-black"
+      className={hovered ?
+        "mr-auto my-auto text-3xl font-sans font-extrabold transition-colors duration-500 ease-linear text-transparent bg-clip-text bg-gradient-to-br from-sd-blue to-sd-cyan"
+        :
+        "mr-auto my-auto text-3xl font-sans font-extrabold text-sd-black"
       }
       onMouseEnter={toggleHover}
       onMouseLeave={toggleHover}
+      onClick={() => {
+        if (hovered) {
+          console.log(hovered)
+          setRefreshed(true);
+          toggleHover();
+        }
+        history.push("/");
+      }}
     >
       Pnyx
     </button>
   )
-}
+})
