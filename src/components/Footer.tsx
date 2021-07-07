339bb345418a4af8a7eb658eb5752e62e22d6734
totuslink <summerbud@totuslink.com>
2021-07-07 14:11:31 +0800

    add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { PrivateAndPoliciesButton } from "./buttons/PrivateAndPoliciesButton";
+import { FeaturesButton } from "./buttons/FeaturesButton"
+import { HowItWorksButton } from "./buttons/HowItWorksButton"
+import { CustomizeDivider } from "./layouts/CustomizeDivider";
+
+interface Props { 
+  className: string;
+}
+
+export const Footer: React.FC<Props> = ({ className }) => {
+  return (
+    <div
+      className={"flex w-screen justify-center flex-row px-6 h-20 flex-shrink-0" + " " + className }
+    >
+      <PrivateAndPoliciesButton />
+      <CustomizeDivider height={6} width={2} />
+      <FeaturesButton />
+      <CustomizeDivider height={6} width={2} />
+      <HowItWorksButton />
+    </div>
+  )
+}
