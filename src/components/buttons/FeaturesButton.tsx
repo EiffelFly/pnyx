4d1d228156a2e88da5efba29bc7184acdf51e503
totuslink <summerbud@totuslink.com>
2021-07-07 10:07:59 +0800

    add history.push into Features and HowItWorks button

--- src/components/buttons/FeaturesButton.tsx
+++ src/components/buttons/FeaturesButton.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import { withRouter } from "react-router-dom";
 
-export const FeaturesButton: React.FC = () => {
+export const FeaturesButton = withRouter(({ history }) => {
   return (
     <button
       className="font-sans font-light text-sd-black text-lg my-auto hover:bg-sd-brcyan hover:bg-opacity-20 px-2 py-0.5 rounded-md"
+      onClick={() => {
+        history.push("/features");
+      }}
     >
       Features
     </button>
   )
-}
+})
--- src/components/buttons/HowItWorksButton.tsx
+++ src/components/buttons/HowItWorksButton.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import { withRouter } from "react-router-dom";
 
-export const HowItWorksButton: React.FC = () => {
+export const HowItWorksButton = withRouter(({ history }) => {
   return (
     <button
       className="font-sans font-light text-sd-black text-lg my-auto hover:bg-sd-brcyan hover:bg-opacity-20 px-2 py-0.5 rounded-md"
+      onClick={() => {
+        history.push("/how-it-works");
+      }}
     >
       How it works
     </button>
   )
-}
+})
