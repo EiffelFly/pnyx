0e0a66db0f22f843b3150412c49d310698928b30
totuslink <summerbud@totuslink.com>
2021-07-11 13:28:02 +0800

    Setup init of i18n

---
+++ src/lib/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import { __prod__ } from "./constants"
+
+const DETECTION_OPTIONS = {
+  order: ["localStorage", "navigator"],
+};
+
+export const initI18n = () => {
+  i18n
+    // import & load translations from -> /public/locales
+    // learn more: https://github.com/i18next/i18next-http-backend
+    .use(Backend)
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    .init({
+      detection: DETECTION_OPTIONS,
+      fallbackLng: "en",
+      debug: !__prod__,
+    })
+}
