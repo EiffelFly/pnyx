0998b029e34fca779d807652f2417f9c820365b6
totuslink <summerbud@totuslink.com>
2021-07-12 13:22:52 +0800

    Cancel Pnyx logo hover effect

--- src/components/buttons/PynxLogoButton.tsx
+++ src/components/buttons/PynxLogoButton.tsx
@@ -1,32 +1,33 @@
-import * as React from "react";
+//import * as React from "react";
 import { withRouter } from "react-router-dom";
 
 //interface Props { }
 
 export const PynxLogoButton = withRouter(({ history }) => {
-  const [hovered, setHovered] = React.useState(false);
-  const [refreshed, setRefreshed] = React.useState(false);
-  const toggleHover = () => {
-    if (refreshed) {
-      setRefreshed(false);
-      return
-    }
-    setHovered(!hovered)
-  };
+  // const [hovered, setHovered] = React.useState(false);
+  // const [refreshed, setRefreshed] = React.useState(false);
+  // const toggleHover = () => {
+  //   if (refreshed) {
+  //     setRefreshed(false);
+  //     return
+  //   }
+  //   setHovered(!hovered)
+  // };
   return (
     <button
-      className={hovered ?
-        "mr-auto my-auto text-3xl font-sans font-extrabold transition-colors duration-500 ease-linear text-transparent bg-clip-text bg-gradient-to-br from-sd-blue to-sd-cyan"
-        :
-        "mr-auto my-auto text-3xl font-sans font-extrabold text-sd-black"
-      }
-      onMouseEnter={toggleHover}
-      onMouseLeave={toggleHover}
+      // className={hovered ?
+      //   "mr-auto my-auto text-3xl font-sans font-extrabold transition-colors duration-500 ease-linear text-transparent bg-clip-text bg-gradient-to-br from-sd-blue to-sd-cyan"
+      //   :
+      //   "mr-auto my-auto text-3xl font-sans font-extrabold text-sd-black"
+      // }
+      className="mr-auto my-auto text-3xl font-sans font-extrabold text-sd-black"
+      //onMouseEnter={toggleHover}
+      //onMouseLeave={toggleHover}
       onClick={() => {
-        if (hovered) {
-          setRefreshed(true);
-          toggleHover();
-        }
+        // if (hovered) {
+        //   setRefreshed(true);
+        //   toggleHover();
+        // }
         history.push("/");
       }}
     >
