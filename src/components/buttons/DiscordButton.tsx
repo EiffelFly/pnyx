700710d0725058646904abc6fb5ea5f6b87b5f38
totuslink <summerbud@totuslink.com>
2021-07-14 09:31:50 +0800

    Update discord link

--- src/components/buttons/DiscordButton.tsx
+++ src/components/buttons/DiscordButton.tsx
@@ -7,7 +7,7 @@ export const DiscordButton: React.FC<Props> = () => {
   return (
     <a
       className="font-sans h-10 flex font-regular text-sd-black text-lg my-auto hover:bg-sd-brcyan hover:bg-opacity-20 px-2 rounded-md"
-      href="https://discord.gg/Qve3u9gahE"
+      href="https://discord.gg/3FM8KXCAmB"
       target="_blank"
       rel="noopener noreferrer"
       onClick={() => {
